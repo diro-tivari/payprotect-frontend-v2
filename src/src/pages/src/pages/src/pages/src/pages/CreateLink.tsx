10c484f774b8aae3dadc9dfd546d4e76accc3eec
diro-tivari <tivari.valbona@gmail.com>
2025-07-20 03:16:21 +0200

    Create CreateLink.tsx

---
+++ src/src/pages/src/pages/src/pages/src/pages/CreateLink.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CreateLink() {
+  return (
+    <div>
+      <h1>Create Payment Link</h1>
+      <p>This page will allow users to create a payment link.</p>
+    </div>
+  );
+}
