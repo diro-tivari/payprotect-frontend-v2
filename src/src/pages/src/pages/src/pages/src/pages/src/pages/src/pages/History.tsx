cff8b7aa7e5274195f11669671e7f05ad447e73b
diro-tivari <tivari.valbona@gmail.com>
2025-07-20 03:18:53 +0200

    Create History.tsx

---
+++ src/src/pages/src/pages/src/pages/src/pages/src/pages/src/pages/History.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function History() {
+  return (
+    <div>
+      <h1>Transaction History</h1>
+      <p>This page will show past transactions.</p>
+    </div>
+  );
+}
