468c0278d0d7ee7700792fa15fd6b031529b9a18
diro-tivari <tivari.valbona@gmail.com>
2025-07-20 03:14:38 +0200

    Create Receive.tsx

---
+++ src/src/pages/src/pages/src/pages/Receive.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Receive() {
+  return (
+    <div>
+      <h1>Receive Money</h1>
+      <p>This page will allow users to receive money.</p>
+    </div>
+  );
+}
