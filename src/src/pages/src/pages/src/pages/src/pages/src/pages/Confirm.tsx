405257dacb70f456ccd24a7783435176baafb539
diro-tivari <tivari.valbona@gmail.com>
2025-07-20 03:18:11 +0200

    Create Confirm.tsx

---
+++ src/src/pages/src/pages/src/pages/src/pages/src/pages/Confirm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Confirm() {
+  return (
+    <div>
+      <h1>Payment Confirmation</h1>
+      <p>This page will confirm the payment.</p>
+    </div>
+  );
+}
+
