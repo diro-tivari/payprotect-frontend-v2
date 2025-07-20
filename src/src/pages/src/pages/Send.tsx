9c8b6143f46138634cfe327d1252e47663b1bee0
diro-tivari <tivari.valbona@gmail.com>
2025-07-20 03:12:40 +0200

    Create Send.tsx

---
+++ src/src/pages/src/pages/Send.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Send() {
+  return (
+    <div>
+      <h1>Send Money</h1>
+      <p>This page will allow users to send money.</p>
+    </div>
+  );
+}
